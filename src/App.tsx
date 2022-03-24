ded257d94c58a0c118aaaea10348ec87a3d85141
jhcode14 <jhsucode@gmail.com>
2022-03-24 14:01:23 -0700

    added router for main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
+    <BrowserRouter>
+      <div>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/introduction">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/">
+            <Route index element={<Landing />} />
+            <Route path="/introduction" element={<Introduction />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path ="/game" element={<Game />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Introduction() {
+  return(
+    <div>introduction</div>
+  )
+}
+
+function AboutUs() {
+  return(
+    <div>about us</div>
+  )
+}
+
+function Game() {
+  return(
+    <div>game</div>
+  )
+}
+
+function Landing() {
+  return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +71,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
